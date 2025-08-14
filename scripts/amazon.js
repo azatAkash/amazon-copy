894dd2cd930b2b3f77c79b0ae015746f9e5b5aac
azatAkash <azatakash14@gmail.com>
2025-08-14 18:38:22 +0500

    add to cart quantity 1 to 10

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -24,7 +24,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -58,7 +58,7 @@ document.querySelectorAll('.js-add-to-cart-button').forEach(
     (button) => {
         button.addEventListener('click', () => {
             const productId = button.dataset.productId;
- 
+            const selectedQuantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
             let matchingItem;
             cart.forEach((item) => {
                 if (item.productId === productId) {
@@ -67,15 +67,16 @@ document.querySelectorAll('.js-add-to-cart-button').forEach(
             })
             
             if (matchingItem) {
-                matchingItem.quantity += 1;
+                matchingItem.quantity += selectedQuantity;
             } else {
                 cart.push({
                     productId,
-                    quantity: 1,
+                    quantity: selectedQuantity,
                 })
                 
             }
-            document.querySelector('.js-cart-quantity').innerHTML  = ++cartQuantity;
+            cartQuantity += selectedQuantity;
+            document.querySelector('.js-cart-quantity').innerHTML  = cartQuantity;
             console.log(cart);
         })
     }
